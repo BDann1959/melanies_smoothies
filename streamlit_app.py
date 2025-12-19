643a21202c9097630cf760be4e5c17c5f97ea884
BDann1959 <Bill.dann1@nhs.net>
2025-12-19 12:51:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,9 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
+        import requests
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.text(smoothiefroot_response)
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered ,' +name_on_order, icon="✅")
